eae02d3e353d88c486b11e5ecae2037776788b5b
To_Do_List.py <santoshkumarkowru@gmail.com>
2025-08-15 17:34:41 +0530

    Even Or Program Modified

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,1 +1,16 @@
- 
+import java.io.*;
+import java.util.*;
+
+class EvenOrOdd{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter The Number:");
+        int n = scanner.nextInt();
+        if(n%2==0){
+            System.out.print("\nGiven Number Is A Even NUmber...\n");
+        }
+        else{
+            System.out.print("\nGiven Number Is A Odd NUmber...\n");
+        }
+    }
+}
