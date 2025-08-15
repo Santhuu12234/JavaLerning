a8e618592258a568da32775bae0ba3ac66e214a8
To_Do_List.py <santoshkumarkowru@gmail.com>
2025-08-15 18:01:29 +0530

    Tables Program Modified

--- Tabels.java
+++ Tabels.java
@@ -1,1 +1,16 @@
- 
+import java.io.*;
+import java.util.Scanner;
+
+class Tables{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nEnter The Table:");
+        int n = scanner.nextInt();
+        System.out.print("Enter Up To:");
+        int m = scanner.nextInt();
+        System.out.print("\nThe "+n+" Table Are:\n");
+        for(int i=1;i<=m;i++){
+            System.out.print(n+"x"+i+"="+(n*i)+"\n");
+        }
+    }
+}
